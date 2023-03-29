c7146c8f1007d29499fc5e6bf3943390ef0b6fd2
3800646 <yona.mellul@etu.umpc.fr>
2023-03-29 15:53:56 +0200

    tme05-06 RENDU

---
+++ glucose-4.2.1/sources/simp/depend.mk
@@ -0,0 +1,42 @@
+/Users/yona/Documents/M1_IMA/IAMSI/glucose-4.2.1/sources/simp/Main.o /Users/yona/Documents/M1_IMA/IAMSI/glucose-4.2.1/sources/simp/Main.or /Users/yona/Documents/M1_IMA/IAMSI/glucose-4.2.1/sources/simp/Main.od /Users/yona/Documents/M1_IMA/IAMSI/glucose-4.2.1/sources/simp/Main.op: \
+  /Users/yona/Documents/M1_IMA/IAMSI/glucose-4.2.1/sources/simp/Main.cc \
+  /Users/yona/Documents/M1_IMA/IAMSI/glucose-4.2.1/sources/simp/../utils/System.h \
+  /Users/yona/Documents/M1_IMA/IAMSI/glucose-4.2.1/sources/simp/../mtl/IntTypes.h \
+  /Users/yona/Documents/M1_IMA/IAMSI/glucose-4.2.1/sources/simp/../utils/ParseUtils.h \
+  /Users/yona/Documents/M1_IMA/IAMSI/glucose-4.2.1/sources/simp/../utils/Options.h \
+  /Users/yona/Documents/M1_IMA/IAMSI/glucose-4.2.1/sources/simp/../mtl/Vec.h \
+  /Users/yona/Documents/M1_IMA/IAMSI/glucose-4.2.1/sources/simp/../mtl/XAlloc.h \
+  /Users/yona/Documents/M1_IMA/IAMSI/glucose-4.2.1/sources/simp/../core/Dimacs.h \
+  /Users/yona/Documents/M1_IMA/IAMSI/glucose-4.2.1/sources/simp/../core/SolverTypes.h \
+  /Users/yona/Documents/M1_IMA/IAMSI/glucose-4.2.1/sources/simp/../mtl/Alg.h \
+  /Users/yona/Documents/M1_IMA/IAMSI/glucose-4.2.1/sources/simp/../mtl/Map.h \
+  /Users/yona/Documents/M1_IMA/IAMSI/glucose-4.2.1/sources/simp/../mtl/Alloc.h \
+  /Users/yona/Documents/M1_IMA/IAMSI/glucose-4.2.1/sources/simp/../simp/SimpSolver.h \
+  /Users/yona/Documents/M1_IMA/IAMSI/glucose-4.2.1/sources/simp/../mtl/Queue.h \
+  /Users/yona/Documents/M1_IMA/IAMSI/glucose-4.2.1/sources/simp/../core/Solver.h \
+  /Users/yona/Documents/M1_IMA/IAMSI/glucose-4.2.1/sources/simp/../mtl/Heap.h \
+  /Users/yona/Documents/M1_IMA/IAMSI/glucose-4.2.1/sources/simp/../core/BoundedQueue.h \
+  /Users/yona/Documents/M1_IMA/IAMSI/glucose-4.2.1/sources/simp/../core/Constants.h \
+  /Users/yona/Documents/M1_IMA/IAMSI/glucose-4.2.1/sources/simp/../mtl/Clone.h \
+  /Users/yona/Documents/M1_IMA/IAMSI/glucose-4.2.1/sources/simp/../core/SolverStats.h
+/Users/yona/Documents/M1_IMA/IAMSI/glucose-4.2.1/sources/simp/SimpSolver.o /Users/yona/Documents/M1_IMA/IAMSI/glucose-4.2.1/sources/simp/SimpSolver.or /Users/yona/Documents/M1_IMA/IAMSI/glucose-4.2.1/sources/simp/SimpSolver.od /Users/yona/Documents/M1_IMA/IAMSI/glucose-4.2.1/sources/simp/SimpSolver.op: \
+  /Users/yona/Documents/M1_IMA/IAMSI/glucose-4.2.1/sources/simp/SimpSolver.cc \
+  /Users/yona/Documents/M1_IMA/IAMSI/glucose-4.2.1/sources/simp/../mtl/Sort.h \
+  /Users/yona/Documents/M1_IMA/IAMSI/glucose-4.2.1/sources/simp/../mtl/Vec.h \
+  /Users/yona/Documents/M1_IMA/IAMSI/glucose-4.2.1/sources/simp/../mtl/IntTypes.h \
+  /Users/yona/Documents/M1_IMA/IAMSI/glucose-4.2.1/sources/simp/../mtl/XAlloc.h \
+  /Users/yona/Documents/M1_IMA/IAMSI/glucose-4.2.1/sources/simp/../simp/SimpSolver.h \
+  /Users/yona/Documents/M1_IMA/IAMSI/glucose-4.2.1/sources/simp/../mtl/Queue.h \
+  /Users/yona/Documents/M1_IMA/IAMSI/glucose-4.2.1/sources/simp/../core/Solver.h \
+  /Users/yona/Documents/M1_IMA/IAMSI/glucose-4.2.1/sources/simp/../mtl/Heap.h \
+  /Users/yona/Documents/M1_IMA/IAMSI/glucose-4.2.1/sources/simp/../mtl/Alg.h \
+  /Users/yona/Documents/M1_IMA/IAMSI/glucose-4.2.1/sources/simp/../utils/Options.h \
+  /Users/yona/Documents/M1_IMA/IAMSI/glucose-4.2.1/sources/simp/../utils/ParseUtils.h \
+  /Users/yona/Documents/M1_IMA/IAMSI/glucose-4.2.1/sources/simp/../core/SolverTypes.h \
+  /Users/yona/Documents/M1_IMA/IAMSI/glucose-4.2.1/sources/simp/../mtl/Map.h \
+  /Users/yona/Documents/M1_IMA/IAMSI/glucose-4.2.1/sources/simp/../mtl/Alloc.h \
+  /Users/yona/Documents/M1_IMA/IAMSI/glucose-4.2.1/sources/simp/../core/BoundedQueue.h \
+  /Users/yona/Documents/M1_IMA/IAMSI/glucose-4.2.1/sources/simp/../core/Constants.h \
+  /Users/yona/Documents/M1_IMA/IAMSI/glucose-4.2.1/sources/simp/../mtl/Clone.h \
+  /Users/yona/Documents/M1_IMA/IAMSI/glucose-4.2.1/sources/simp/../core/SolverStats.h \
+  /Users/yona/Documents/M1_IMA/IAMSI/glucose-4.2.1/sources/simp/../utils/System.h
